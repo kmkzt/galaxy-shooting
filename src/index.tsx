e858b4fcaff93ad5f3efa81b0d960b3ba880fb21
kmkzt <info.pscreator@gmail.com>
2019-10-14 23:26:32 +0900

    refactor: refactor comments

--- src/index.tsx
+++ src/index.tsx
@@ -29,39 +29,72 @@ const TRANSLATE_UNIT = 0.05
 const CAMERA_MOVE_UNIT = 20
 const ROTATE_UNIT = 0.1
 
-// const FRAME_X = 500
-// const FRAME_Y = 500
-const FRAME_X = innerWidth
-const FRAME_Y = innerHeight
-
-const ASPECT_RATIO = FRAME_X / FRAME_Y
-const FAR = 10
-
 /**
- * Object
- */
-let spaceShip: Mesh
-let spaceShip_rotate: boolean = false
-
-let boxs: Object3D[] = []
-
-/**
- * Controler
+ * DEV TOOLS
  */
 const stats = new Stats()
+const gui = new dat.GUI()
+const setDataGui = (data: object, g: dat.GUI) => {
+  for (let key in data) {
+    switch (typeof data[key]) {
+      case 'object':
+        // console.log(key, typeof spaceShip[key])
+        let paramGui = g.addFolder(key)
+        for (let param in data[key]) {
+          if (typeof data[key][param] === 'function') continue
+          if (typeof data[key][param] === 'object') continue
+          paramGui.add(data[key], param)
+        }
+        break
+      case 'number':
+      case 'string':
+        g.add(data, key)
+        break
+    }
+  }
+}
+/**
+ * Renderer
+ */
 const canvasFrame = document.getElementById('drawarea') as HTMLCanvasElement
-
-const camera = new PerspectiveCamera(200, ASPECT_RATIO, 0.02, FAR)
-const scene = new Scene()
+const FRAME_X = innerWidth
+const FRAME_Y = innerHeight
+// const FRAME_X = 500
+// const FRAME_Y = 500
 
 const renderer = new WebGLRenderer({
   antialias: true,
   canvas: canvasFrame
 })
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(FRAME_X, FRAME_Y)
 
 /**
- * Generate SpaceShip
+ * Camera
  */
+const ASPECT_RATIO = FRAME_X / FRAME_Y
+const FOV = 200
+const FAR = 10
+const NEAR = 0.02
+const DEFAULT_DISTANCE_Z = 1
+
+const camera = new PerspectiveCamera(FOV, ASPECT_RATIO, NEAR, FAR)
+camera.position.z = DEFAULT_DISTANCE_Z
+/**
+ * Scene
+ */
+const scene = new Scene()
+scene.background = new Color(0x000000)
+scene.fog = new Fog(0x000000, 50, 2000)
+// scene.fog = new Fog(0xffffff, 0, 750)
+const light = new HemisphereLight(0xeeeeff, 0x777788, 0.75)
+light.position.set(0.5, 1, 0.75)
+scene.add(light)
+/**
+ * SpaceShip Configuration
+ */
+let spaceShip: Mesh
+let spaceShip_rotate: boolean = false
 const genSpaceShip = (s: Scene) => {
   const geometry: Geometry = new BoxGeometry(1, 0.2, 0.2)
   const material: Material = new MeshNormalMaterial()
@@ -69,9 +102,12 @@ const genSpaceShip = (s: Scene) => {
   spaceShip = new Mesh(geometry, material)
   s.add(spaceShip)
 }
+const switchSpeceShipRotate = () => (spaceShip_rotate = !spaceShip_rotate)
+
 /**
  * Generate box
  */
+let boxs: Object3D[] = []
 const genBox = (s: Scene) => {
   const boxBufferGeometry = new BoxBufferGeometry(1, 1, 1)
   const boxGeometry = boxBufferGeometry.toNonIndexed() // ensure each face has unique vertices
@@ -104,31 +140,12 @@ const genBox = (s: Scene) => {
     boxs.push(box)
   }
 }
-
+// TODO: Fix check no block
+const isNoBox = () => camera.position.z < -camera.far
+/**
+ * Init
+ */
 const init = () => {
-  camera.position.z = 1
-
-  /**
-   * background color
-   */
-  scene.background = new Color(0x000000)
-  /**
-   * Fog
-   */
-  scene.fog = new Fog(0x000000, 50, 2000)
-  /**
-   * Light
-   */
-  const light = new HemisphereLight(0xeeeeff, 0x777788, 0.75)
-  light.position.set(0.5, 1, 0.75)
-  scene.add(light)
-
-  /**
-   * TODO: adjust box scene
-   */
-  // scene = new Scene()
-  // scene.background = new Color(0xffffff)
-  scene.fog = new Fog(0xffffff, 0, 750)
   /**
    * generate space ship
    */
@@ -140,48 +157,16 @@ const init = () => {
   genBox(scene)
 
   /**
-   * Renderer config
-   */
-  renderer.setPixelRatio(window.devicePixelRatio)
-  renderer.setSize(FRAME_X, FRAME_Y)
-
-  /**
    * DEV TOOLS
    */
   document.body.appendChild(stats.dom)
 
-  const gui = new dat.GUI()
+  // TODO: FIX controlable object
   setDataGui(camera, gui.addFolder('camera'))
   setDataGui(scene, gui.addFolder('scene'))
   setDataGui(light, gui.addFolder('light'))
   setDataGui(spaceShip, gui.addFolder('spaceShip'))
 }
-
-const setDataGui = (data: object, g: dat.GUI) => {
-  for (let key in data) {
-    switch (typeof data[key]) {
-      case 'object':
-        // console.log(key, typeof spaceShip[key])
-        let paramGui = g.addFolder(key)
-        for (let param in data[key]) {
-          if (typeof data[key][param] === 'function') continue
-          if (typeof data[key][param] === 'object') continue
-          paramGui.add(data[key], param)
-        }
-        break
-      case 'number':
-      case 'string':
-        g.add(data, key)
-        break
-    }
-  }
-}
-
-/**
- * Check Block
- * TODO: Fix check no block
- */
-const isNoBlock = () => camera.position.z < -10
 
 /**
  * Animation loop
@@ -193,10 +178,10 @@ const animate = () => {
     spaceShip.rotation.z += ROTATE_UNIT
   }
 
-  if (isNoBlock()) {
+  if (isNoBox()) {
     scene.remove(...boxs)
     genBox(scene)
-    camera.position.z = 1
+    camera.position.z = DEFAULT_DISTANCE_Z
     spaceShip.position.z = 0
   }
   /**
@@ -215,30 +200,32 @@ init()
 animate()
 
 /**
- *
+ * CLICK ACTION
  */
 canvasFrame.addEventListener('click', () => {
-  spaceShip_rotate = !spaceShip_rotate
+  switchSpeceShipRotate()
 })
 
 /**
- * MOUSEMOVE HANDLER
+ * MOUSEMOVE ACTION
  */
 canvasFrame.addEventListener('mousemove', (e: MouseEvent) => {
-  const canvasRect = canvasFrame.getBoundingClientRect()
   /**
-   * frame point
+   * mosemove point
    */
-  const x =
+  const canvasRect = canvasFrame.getBoundingClientRect()
+  const mousemove_x =
     ((canvasRect.width - e.clientX - FRAME_X / 2) * camera.aspect) /
     canvasRect.width
-  const y = (e.clientY - canvasRect.height + FRAME_Y / 2) / canvasRect.height
+  const mousemove_y =
+    (e.clientY - canvasRect.height + FRAME_Y / 2) / canvasRect.height
 
   /**
    * SpaceShip move
    */
-  spaceShip.position.x = x * camera.far
-  spaceShip.position.y = y * camera.far
+  const distanceSpaceshipFromCamera = camera.position.z - spaceShip.position.z
+  spaceShip.position.x = mousemove_x * camera.far * distanceSpaceshipFromCamera
+  spaceShip.position.y = mousemove_y * camera.far * distanceSpaceshipFromCamera
 })
 
 const isKeycode = (key: number): key is Keyboard =>
@@ -253,50 +240,38 @@ window.addEventListener('keydown', (ev: KeyboardEvent) => {
 
 const keyboardAction = (key: Keyboard) => {
   switch (key) {
-    /**
-     * spaceShip POSITION Y
-     */
     case Keyboard.UP_ARROW:
       spaceShip.position.y -= TRANSLATE_UNIT
       break
     case Keyboard.DOWN_ARROW:
       spaceShip.position.y += TRANSLATE_UNIT
       break
-    /**
-     * BOX POSITION X
-     */
     case Keyboard.RIGHT_ARROW:
       spaceShip.position.x -= TRANSLATE_UNIT
       break
     case Keyboard.LEFT_ARROW:
       spaceShip.position.x += TRANSLATE_UNIT
       break
-    /**
-     * CAMERA POSITION Y
-     */
     case Keyboard.KEY_U:
       camera.position.y -= CAMERA_MOVE_UNIT
       break
     case Keyboard.KEY_D:
       camera.position.y += CAMERA_MOVE_UNIT
       break
-    /**
-     * CAMERA POSITION X
-     */
     case Keyboard.KEY_L:
       camera.position.x -= CAMERA_MOVE_UNIT
       break
     case Keyboard.KEY_R:
       camera.position.x += CAMERA_MOVE_UNIT
       break
-    /**
-     * CAMERA POSITION Z
-     */
     case Keyboard.KEY_N:
       camera.position.z -= CAMERA_MOVE_UNIT
       break
     case Keyboard.KEY_F:
       camera.position.z += CAMERA_MOVE_UNIT
+      break
+    case Keyboard.SPACE:
+      switchSpeceShipRotate()
       break
   }
 }
